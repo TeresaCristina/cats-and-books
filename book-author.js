39c992fc8793650a08c39af3d195d457668eb173
TeresaComputer <tcpaixao-costa@myseneca.ca>
2023-08-31 10:33:09 +0100

    add reviews, add countries

--- book-author.js
+++ book-author.js
@@ -2,8 +2,9 @@ const apiLibraryAuthor = 'https://openlibrary.org/authors/';
 const authors = [
     "OL2645201A",
     "OL5499254A",
+    "OL29464A"
 ];
-var apiAuthors = apiLibraryAuthor + authors[1] + '.json';
+var apiAuthors = apiLibraryAuthor + authors[2] + '.json';
 var errorMessage = "This is supposed to be the name of a person.";
 
 
--- book-details.js
+++ book-details.js
@@ -2,16 +2,17 @@ const apiLibrary = 'https://openlibrary.org/works/';
 const books = [
     "OL15611821W",
     "OL19980028W",
+    "OL102370W"
 ];
-var apiBooks = apiLibrary + books[1] + '.json';
+var apiBooks = apiLibrary + books[2] + '.json';
 var errorTitle = "This is supposed to be a book title. But something happened...maybe it was a cat...";
 var errorDescription = "This is supposed to be a book description. But something happened...maybe it was a cat...";
 
 fetch(apiBooks).then(response => {
     return response.json();
 }).then(data => {
-    document.getElementById("book-title").innerHTML = data.title,
-        document.getElementById("book-description").innerHTML = data.description.value
+    document.getElementById("book-title").innerHTML = data.title
+        //document.getElementById("book-description").innerHTML = data.description.value
 }
 ).catch(err => {
     document.getElementById("book-title").innerHTML = errorTitle,
